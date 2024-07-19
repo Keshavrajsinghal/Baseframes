b0869b62cc7f79616224e08e463e041e1eb675b7
keshav <keshav17@berkeley.edu>
2024-07-19 15:27:45 -0700

    api route

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,1 +1,1 @@
-export const NEXT_PUBLIC_URL = process.env.NODE_ENV == 'development' ? 'http://localhost:3000' : 'https://baseframes.vercel.app';
+export const NEXT_PUBLIC_URL = process.env.NODE_ENV == 'development' ? 'http://localhost:3000' : 'https://baseframes.vercel.app/';
